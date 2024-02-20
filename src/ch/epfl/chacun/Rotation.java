3f8cee1262ec8256755e0f9842f49f4768e1d396
ValerHans <valerio.desantis@epfl.ch>
2024-02-20 09:20:47 +0100

    Update Rotation.java

--- src/ch/epfl/chacun/Rotation.java
+++ src/ch/epfl/chacun/Rotation.java
@@ -3,28 +3,23 @@ package ch.epfl.chacun;
 import java.util.List;
 
 public enum Rotation {
-
     NONE,
     RIGHT,
     HALF_TURN,
     LEFT;
 
     public static final List<Rotation> ALL = List.of(Rotation.values());
-
+    public static final int COUNT = ALL.size();
 
     public int degreesCW(){
         return this.ordinal()*90;
     }
-    public Rotation add(Rotation that){
+    public Rotation add(Rotation that) {
         return ALL.get((this.ordinal() + that.ordinal()) % COUNT);
     }
-
-    public static final int COUNT = ALL.size();
-
-
-    //We use the mod 4 operation to ensure that this.ordinal being 0
-    //works as expected
     public Rotation negated(){
+        //The mod 4 operation is used to ensure that this.ordinal being 0
+        //works as expected
         return ALL.get((COUNT - this.ordinal()) % 4);
     }
 
