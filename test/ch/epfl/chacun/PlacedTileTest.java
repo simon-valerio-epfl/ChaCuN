e0b7507b706890e22d4adc4c62e095ed0a917a8c
Androz 2091 <androz2091@gmail.com>
2024-02-23 17:15:43 +0100

    add tests

--- test/ch/epfl/chacun/PlacedTileTest.java
+++ test/ch/epfl/chacun/PlacedTileTest.java
@@ -3,7 +3,10 @@ package ch.epfl.chacun;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class PlacedTileTest {
 
@@ -47,6 +50,30 @@ class PlacedTileTest {
         assertThrows(IllegalArgumentException.class, () -> {
             placedTile.zoneWithId(100000);
         });
+    }
+
+    @Test
+    public void testTypeZonesReturnsCorrectZones() {
+        Zone.Meadow meadow = new Zone.Meadow(613, List.of(new Animal(6131, Animal.Kind.AUROCHS)), Zone.Meadow.SpecialPower.HUNTING_TRAP);
+        Zone.Meadow meadow2 = new Zone.Meadow(614, List.of(new Animal(6141, Animal.Kind.MAMMOTH)), null);
+        Zone.Forest forest2 = new Zone.Forest(615, Zone.Forest.Kind.PLAIN);
+        Zone.Forest forest = new Zone.Forest(612, Zone.Forest.Kind.WITH_MENHIR);
+        TileSide forestSide = new TileSide.Forest(forest);
+        TileSide meadowSide = new TileSide.Meadow(meadow);
+        TileSide forestSide2 = new TileSide.Forest(forest2);
+        TileSide meadowSide2 = new TileSide.Meadow(meadow2);
+        Tile tile = new Tile(1, Tile.Kind.START, forestSide, meadowSide, forestSide2, meadowSide2);
+        PlayerColor Habib = PlayerColor.RED;
+
+        PlacedTile placedTile = new PlacedTile(tile, Habib, Rotation.RIGHT, new Pos(0, 0));
+
+        final Set<Zone> expectedForestSet = new HashSet<>(List.of(new Zone[]{forest, forest2}));
+        assertEquals(expectedForestSet, placedTile.forestZones());
+
+        final Set<Zone> expectedMeadowSet = new HashSet<>(List.of(new Zone[]{meadow2, meadow}));
+        assertEquals(expectedMeadowSet, placedTile.meadowZones());
+
+        assertEquals(new HashSet<>(), placedTile.riverZones());
     }
 
     @Test
