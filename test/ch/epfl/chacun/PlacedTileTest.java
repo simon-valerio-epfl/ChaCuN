7dadf5294a5e0c71a291de69a86f87b77208c8b9
Androz 2091 <androz2091@gmail.com>
2024-02-23 16:52:45 +0100

    add more tests

--- test/ch/epfl/chacun/PlacedTileTest.java
+++ test/ch/epfl/chacun/PlacedTileTest.java
@@ -29,8 +29,24 @@ class PlacedTileTest {
     }
 
     @Test
-    public void test() {
+    public void testZoneWithIdReturnsCorrectZone() {
+        Zone.Meadow meadow = new Zone.Meadow(613, List.of(new Animal(6131, Animal.Kind.AUROCHS)), Zone.Meadow.SpecialPower.HUNTING_TRAP);
+        Zone.Meadow meadow2 = new Zone.Meadow(614, List.of(new Animal(6141, Animal.Kind.MAMMOTH)), null);
+        Zone.Forest forest2 = new Zone.Forest(615, Zone.Forest.Kind.PLAIN);
+        Zone.Forest forest = new Zone.Forest(612, Zone.Forest.Kind.WITH_MENHIR);
+        TileSide forestSide = new TileSide.Forest(forest);
+        TileSide meadowSide = new TileSide.Meadow(meadow);
+        TileSide forestSide2 = new TileSide.Forest(forest2);
+        TileSide meadowSide2 = new TileSide.Meadow(meadow2);
+        Tile tile = new Tile(1, Tile.Kind.START, forestSide, meadowSide, forestSide2, meadowSide2);
+        PlayerColor Habib = PlayerColor.RED;
 
+        PlacedTile placedTile = new PlacedTile(tile, Habib, Rotation.RIGHT, new Pos(0, 0));
+
+        assertEquals(forest, placedTile.zoneWithId(612));
+        assertThrows(IllegalArgumentException.class, () -> {
+            placedTile.zoneWithId(100000);
+        });
     }
 
 }
