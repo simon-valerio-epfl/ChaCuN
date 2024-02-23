67cd800347251accd1c9be5237ab0e09bfad0871
Androz 2091 <androz2091@gmail.com>
2024-02-23 17:38:59 +0100

    add more tests

--- test/ch/epfl/chacun/PlacedTileTest.java
+++ test/ch/epfl/chacun/PlacedTileTest.java
@@ -148,8 +148,32 @@ class PlacedTileTest {
         set2.add(new Occupant(Occupant.Kind.PAWN, 614));
         set2.add(new Occupant(Occupant.Kind.PAWN, 612));
 
-
         assertEquals(set2, placedTile3.potentialOccupants());
+    }
+
+    @Test
+    public void testWithOccupantThrows() {
+        Zone.Meadow meadow = new Zone.Meadow(613, List.of(new Animal(6131, Animal.Kind.AUROCHS)), Zone.Meadow.SpecialPower.HUNTING_TRAP);
+        Zone.Meadow meadow2 = new Zone.Meadow(614, List.of(new Animal(6141, Animal.Kind.MAMMOTH)), null);
+        Zone.Forest forest2 = new Zone.Forest(615, Zone.Forest.Kind.PLAIN);
+        Zone.Forest forest = new Zone.Forest(612, Zone.Forest.Kind.WITH_MENHIR);
+        TileSide forestSide = new TileSide.Forest(forest);
+        TileSide meadowSide = new TileSide.Meadow(meadow);
+        TileSide forestSide2 = new TileSide.Forest(forest2);
+        TileSide meadowSide2 = new TileSide.Meadow(meadow2);
+        Tile tile = new Tile(1, Tile.Kind.START, forestSide, meadowSide, forestSide2, meadowSide2);
+        PlayerColor Habib = PlayerColor.RED;
+
+        PlacedTile placedTile = new PlacedTile(tile, Habib, Rotation.RIGHT, new Pos(0, 0));
+
+        Occupant occupant = new Occupant(Occupant.Kind.PAWN, 613);
+        assertThrows(IllegalArgumentException.class, () -> {
+            PlacedTile withOccupant = placedTile.withOccupant(occupant);
+            withOccupant.withOccupant(new Occupant(Occupant.Kind.PAWN, 613));
+        });
+
+        PlacedTile withOccupant = placedTile.withOccupant(new Occupant(Occupant.Kind.PAWN, 613));
+        assertEquals(occupant, withOccupant.occupant());
     }
 
 }
