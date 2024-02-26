68d27e4e5548da7d2bddfb730d8279cad9436c12
Androz 2091 <androz2091@gmail.com>
2024-02-26 21:59:59 +0100

    remove paths

--- scripts/run_tests/run_tests.sh
+++ scripts/run_tests/run_tests.sh
@@ -23,15 +23,15 @@ done
 # Supprimer le premier caractère ':' du CLASSPATH
 CLASSPATH=${CLASSPATH:1}
 
-/usr/bin/find "${ROOT_DIR}/src/ch/epfl/chacun" -name "*.java" > src_files.txt
-/usr/bin/find "${ROOT_DIR}/test/ch/epfl/chacun" -name "*.java" > test_files.txt
+find "${ROOT_DIR}/src/ch/epfl/chacun" -name "*.java" > src_files.txt
+find "${ROOT_DIR}/test/ch/epfl/chacun" -name "*.java" > test_files.txt
 
-/usr/bin/rm -rf ./out
+rm -rf ./out
 
 # build production class
-/usr/bin/javac -d out/production/classes @src_files.txt
+javac -d out/production/classes @src_files.txt
 # build test class
-/usr/bin/javac -d out/test/classes -classpath out/production/classes:"$CLASSPATH" @test_files.txt
-/usr/bin/java -jar junit-platform-console-standalone-1.10.2.jar execute -cp out/production/classes:out/test/classes --select-package ch.epfl.chacun
+javac -d out/test/classes -classpath out/production/classes:"$CLASSPATH" @test_files.txt
+java -jar junit-platform-console-standalone-1.10.2.jar execute -cp out/production/classes:out/test/classes --select-package ch.epfl.chacun
 
 echo "Compilation terminée."
