770c3f714267efa1e978dc350ad77f792fc5a50b
ValerHans <valerio.desantis@epfl.ch>
2024-02-27 18:26:05 +0100

    Doc Area

--- src/ch/epfl/chacun/Area.java
+++ src/ch/epfl/chacun/Area.java
@@ -6,28 +6,47 @@ import java.util.stream.Collectors;
 /**
  * Represents an area in the game.
  *
- * @param zones non-null, the zones in the area
- * @param occupants non-null, the occupants in the area
- * @param openConnections non-negative, the number of open connections in the area
- * @param <Z> the type of zone in the area
+ * @param zones the zones forming the area
+ * @param occupants the players having put an occupant in one of the area's zones
+ * @param openConnections non-negative, the number of open connections the area has
+ * @param <Z> the type of the zones forming the area
  *
  * @author Valerio De Santis (373247)
  * @author Simon Lefort (371918)
  */
 public record Area<Z extends Zone> (Set<Z> zones, List<PlayerColor> occupants, int openConnections) {
 
+    /**
+     * Checks that the given openConnections is non-negative, and initializes the area
+     * with the given zones,occupants and openConnections.
+     *
+     * @param zones the zones forming the area
+     * @param occupants the players having put an occupant in one of the area's zones
+     * @param openConnections non-negative, the number of open connections the area has
+     */
     public Area  {
         Preconditions.checkArgument(openConnections >= 0);
         zones = Set.copyOf(zones);
         occupants = List.copyOf(sortOccupants(occupants));
     }
 
+    /**
+     * Sorts the given list of players basing on the order of their colours and
+     * returns a new list containing the same players, but sorted.
+     *
+     * @param occupants the list of players to sort
+     */
     private List<PlayerColor> sortOccupants(List<PlayerColor> occupants) {
         List<PlayerColor> sortedOccupants = new ArrayList<>(occupants);
         Collections.sort(sortedOccupants);
         return sortedOccupants;
     }
 
+    /**
+     * Checks whether a certain forest area contains a zone with a menhir.
+     * @param forest the forest area to check
+     * @return whether the given area has a menhir
+     */
     public static boolean hasMenhir(Area<Zone.Forest> forest) {
         // creates a stream containing the zones in the Forest area and checks whether one of them has a menhir
         return forest.zones()
@@ -37,6 +56,11 @@ public record Area<Z extends Zone> (Set<Z> zones, List<PlayerColor> occupants, i
                 .anyMatch(zone -> zone.kind() == Zone.Forest.Kind.WITH_MENHIR);
     }
 
+    /**
+     * Counts the number of mushroom groups in a certain forest area.
+     * @param forest the forest area to check
+     * @return the number of mushroom groups in the given area
+     */
     public static int mushroomGroupCount(Area<Zone.Forest> forest){
         // by default, Stream.count() returns a long value
          return (int) forest.zones()
@@ -46,7 +70,15 @@ public record Area<Z extends Zone> (Set<Z> zones, List<PlayerColor> occupants, i
                  .filter(zone -> zone.kind() == Zone.Forest.Kind.WITH_MUSHROOMS)
                  .count();
     }
-
+    /**
+     * Generates a set containing the animals in a certain meadow area,
+     *  excluding the ones in the given set,
+     * (which are considered to be eaten in the game this class was thought for).
+     *
+     * @param meadow the meadow area to check
+     * @param cancelledAnimals the set of animals to exclude
+     * @return the set of animals in the given area, excluding the ones who have been cancelled
+     */
     public static Set<Animal> animals(Area<Zone.Meadow> meadow, Set<Animal> cancelledAnimals) {
         return meadow.zones()
                 .stream()
@@ -65,7 +97,11 @@ public record Area<Z extends Zone> (Set<Z> zones, List<PlayerColor> occupants, i
                 // therefore contain all the animals living in the area
                 .collect(Collectors.toSet());
     }
-
+    /**
+     * Counts the number of fishes in a certain river area and in the lakes connected to it.
+     * @param river the river area to check
+     * @return the number of fishes in the given river area and in the lakes connected to it
+     */
     public static int riverFishCount(Area<Zone.River> river) {
         Set<Zone.Lake> addedLakes = new HashSet<Zone.Lake>();
         return river.zones()
@@ -84,14 +120,21 @@ public record Area<Z extends Zone> (Set<Z> zones, List<PlayerColor> occupants, i
                 .sum();
     }
 
+    /**
+     * Checks whether the current instance of area is closed.
+     * @return  whether the current instance of area is closed
+     */
     public boolean isClosed() {
         return openConnections == 0;
     }
-
+    /**
+     * Checks whether there is some occupant in the current instance of area.
+     * @return  whether the current instance of area is occupied
+     */
     public boolean isOccupied() {
         return !occupants.isEmpty();
     }
-
+    
     public Set<PlayerColor> majorityOccupants() {
 
     }
