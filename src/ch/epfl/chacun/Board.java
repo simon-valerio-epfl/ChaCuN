7128f65cc537df92c9cab627456190de0b57ba07
ValerHans <valerio.desantis@epfl.ch>
2024-03-13 19:30:16 +0100

    fix imports

--- src/ch/epfl/chacun/Board.java
+++ src/ch/epfl/chacun/Board.java
@@ -1,8 +1,13 @@
 package ch.epfl.chacun;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
 
 public final class Board {
 
