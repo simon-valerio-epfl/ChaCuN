788d70806c8bdc5c8d931746b831ca1b5c303182
Androz 2091 <androz2091@gmail.com>
2024-03-15 12:30:57 +0100

    fix submit

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -33,9 +33,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "Thiej7ai";
+    private static final String TOKEN_1 = "ooquei7P";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "aewieSh8";
+    private static final String TOKEN_2 = "Eigiev2i";
     // Noms des éventuels fichiers Java additionnels à inclure (p.ex. "MyClass.java")
     private static final List<String> ADDITIONAL_FILES =
             List.of();
