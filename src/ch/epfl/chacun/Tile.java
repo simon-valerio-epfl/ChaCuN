ee100bc6cc08575b2164dda47e58b9482e0b96da
Androz 2091 <androz2091@gmail.com>
2024-04-08 15:42:50 +0200

    fix

--- src/ch/epfl/chacun/Tile.java
+++ src/ch/epfl/chacun/Tile.java
@@ -44,7 +44,7 @@ public record Tile(int id, Kind kind, TileSide n, TileSide e, TileSide s, TileSi
      * @return all the zones of the tile
      */
     public Set<Zone> zones() {
-        Set<Zone> zones = new HashSet<>();
+        Set<Zone> zones = new HashSet<>(sideZones());
         for (Zone sideZone : sideZones()) {
             if (sideZone instanceof Zone.River river) {
                 if (river.hasLake()) zones.add(river.lake());
