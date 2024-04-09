0dfcc4a7ac7125599cbd5a2f1beb9236054936e0
Androz 2091 <androz2091@gmail.com>
2024-04-09 10:12:30 +0200

    add color map

--- src/ch/epfl/chacun/gui/ColorMap.java
+++ src/ch/epfl/chacun/gui/ColorMap.java
@@ -1,4 +1,35 @@
 package ch.epfl.chacun.gui;
 
+import ch.epfl.chacun.PlayerColor;
+import javafx.scene.paint.Color;
+
+import java.util.Map;
+
 public class ColorMap {
+
+    private ColorMap() {}
+
+
+    private final static Map<PlayerColor, Color> fillColorMap = Map.of(
+        PlayerColor.RED, Color.RED,
+        PlayerColor.BLUE, Color.BLUE,
+        PlayerColor.GREEN, Color.LIME,
+        PlayerColor.YELLOW, Color.YELLOW
+    );
+
+    private final static Map<PlayerColor, Color> strokeColorMap = Map.of(
+        PlayerColor.RED, Color.WHITE,
+        PlayerColor.BLUE, Color.WHITE,
+        PlayerColor.GREEN, fillColor(PlayerColor.GREEN).deriveColor(0, 1, 0.6, 1),
+        PlayerColor.YELLOW, fillColor(PlayerColor.YELLOW).deriveColor(0, 1, 0.6, 1)
+    );
+
+    public static Color fillColor(PlayerColor playerColor) {
+        return fillColorMap.get(playerColor);
+    }
+
+    public static Color strokeColor (PlayerColor playerColor) {
+        return strokeColorMap.get(playerColor);
+    }
+
 }
