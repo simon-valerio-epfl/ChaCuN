c7d9eba6d8fb52cc7151ca21d38f45786c1d1dbc
Androz 2091 <androz2091@gmail.com>
2024-04-09 11:33:46 +0200

    include preview everywhere

--- scripts/run_tests/run_tests.sh
+++ scripts/run_tests/run_tests.sh
@@ -12,11 +12,29 @@ find "${ROOT_DIR}/test/ch/epfl/chacun" -name "*.java" > test_files.txt
 
 rm -rf ./out
 
-# build production class
-javac -d out/production/classes @src_files.txt --enable-preview --release 21 --module-path ${JAVAFX_SDK_PATH}/lib --add-modules javafx.controls,javafx.fxml
-# build test class
-javac -d out/test/classes -classpath out/production/classes:junit-platform-console-standalone-1.10.2.jar @test_files.txt --enable-preview --release 21 --module-path ${JAVAFX_SDK_PATH}/lib --add-modules javafx.controls,javafx.fxml
-CSV_FILE_PATH="${ROOT_DIR}/test/ch/epfl/chacun/tuiles.csv" java -jar junit-platform-console-standalone-1.10.2.jar execute -cp out/production/classes:out/test/classes: --select-package ch.epfl.chacun --reports-dir reports
+# Assurez-vous que cette variable pointe vers le dossier lib du SDK JavaFX téléchargé
+JAVAFX_LIB_PATH="${JAVAFX_SDK_PATH}/lib"
+
+# Compilation des classes de production avec JavaFX
+javac --enable-preview \
+      --module-path "${JAVAFX_LIB_PATH}" \
+      --add-modules javafx.controls,javafx.fxml \
+      -d out/production/classes @src_files.txt
+
+# Compilation des classes de test
+javac --enable-preview \
+      --module-path "${JAVAFX_LIB_PATH}" \
+      --add-modules javafx.controls,javafx.fxml \
+      -d out/test/classes -classpath out/production/classes:junit-platform-console-standalone-1.10.2.jar @test_files.txt
+
+# Exécution des tests
+CSV_FILE_PATH="${ROOT_DIR}/test/ch/epfl/chacun/tuiles.csv" java --enable-preview \
+      --module-path "${JAVAFX_LIB_PATH}" \
+      --add-modules javafx.controls,javafx.fxml \
+      -jar junit-platform-console-standalone-1.10.2.jar \
+      -cp out/production/classes:out/test/classes: \
+      --select-package ch.epfl.chacun \
+      --reports-dir reports
 
 grep -q "failures=\"0\"" reports/TEST-junit-jupiter.xml && grep -q "errors=\"0\"" reports/TEST-junit-jupiter.xml || exit 1
 
