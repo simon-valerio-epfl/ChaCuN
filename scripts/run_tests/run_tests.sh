2e91c1e4b273a46f0e6da51501a40a5018567397
Androz 2091 <androz2091@gmail.com>
2024-04-11 17:17:19 +0200

    fix scrip

--- scripts/run_tests/run_tests.sh
+++ scripts/run_tests/run_tests.sh
@@ -17,6 +17,8 @@ rm -rf ./out
 # Assurez-vous que cette variable pointe vers le répertoire lib de JavaFX SDK
 JAVAFX_LIB_PATH="${JAVAFX_SDK_PATH}/lib"
 
+echo $JAVAFX_LIB_PATH
+
 # Compilation avec prise en compte de JavaFX et des fonctionnalités de preview
 javac --enable-preview --release 21 \
       --module-path "${JAVAFX_LIB_PATH}" \
@@ -29,7 +31,7 @@ javac --enable-preview --release 21 \
       -d out/test/classes -classpath out/production/classes:junit-platform-console-standalone-1.10.2.jar @test_files.txt
 
 # Exécution des tests
-CSV_FILE_PATH="${ROOT_DIR}/test/ch/epfl/chacun/tuiles.csv" java --enable-preview --release 21 \
+CSV_FILE_PATH="${ROOT_DIR}/test/ch/epfl/chacun/tuiles.csv" java --enable-preview  \
       --module-path "${JAVAFX_LIB_PATH}" \
       --add-modules javafx.controls,javafx.fxml \
       -jar junit-platform-console-standalone-1.10.2.jar \
