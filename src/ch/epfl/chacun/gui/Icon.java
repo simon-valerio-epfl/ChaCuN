9bcb5621783f5b5a2da028efa9411c0465840f8d
Androz 2091 <androz2091@gmail.com>
2024-04-16 08:18:09 +0200

    fix static

--- src/ch/epfl/chacun/gui/Icon.java
+++ src/ch/epfl/chacun/gui/Icon.java
@@ -7,7 +7,9 @@ import javafx.scene.shape.SVGPath;
 
 import java.util.Map;
 
-public class Icon {
+public final class Icon {
+
+    private Icon () {}
 
     private final static String PAWN_SVG = "M -10 10 H -4 L 0 2 L 6 10 H 12 L 5 0 L 12 -2 L 12 -4 L 6 -6 L 6 -10 L 0 -10 L -2 -4 L -6 -2 L -8 -10 L -12 -10 L -8 6 Z";
     private final static String HUT_SVG = "M -8 10 H 8 V 2 H 12 L 0 -10 L -12 2 H -8 Z";
@@ -16,7 +18,7 @@ public class Icon {
         Occupant.Kind.HUT, HUT_SVG
     );
 
-    public Node newFor(PlayerColor playerColor, Occupant.Kind kind) {
+    public static Node newFor(PlayerColor playerColor, Occupant.Kind kind) {
         SVGPath svgPath = new SVGPath();
         svgPath.setContent(SVG_MAP.get(kind));
         svgPath.setFill(ColorMap.fillColor(playerColor));
