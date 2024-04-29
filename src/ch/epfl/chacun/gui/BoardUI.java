8573a41171ca9365d3710b0570d81265b26cd4d6
Androz 2091 <androz2091@gmail.com>
2024-04-29 12:48:47 +0200

    syntax: remove extra g

--- src/ch/epfl/chacun/gui/BoardUI.java
+++ src/ch/epfl/chacun/gui/BoardUI.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
-g
+
 public final class BoardUI {
     /**
      * This is a utility class and therefore is not instantiable
